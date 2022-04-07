c48d97a6abe760bae7e5cc31be6040926be2618c
Diana <82055771+diana-popova@users.noreply.github.com>
2022-04-07 20:06:43 +0300

    Add files via upload

---
+++ lab2.4/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("factorial = " + fact(5));
+    }
+
+    public static int fact(int n){
+        int result;
+        if(n == 1)
+            return 1;
+        else{
+            result = fact(n-1)*n;
+            return result;
+        }
+    }
+}
